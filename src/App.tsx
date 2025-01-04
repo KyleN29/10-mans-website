ce9a60e938075203c17dea4e3182f725ec12adf9
Nate Nevois <nnnevwa630@gmail.com>
2025-01-04 13:25:47 -0600

    Format code and implement navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { StrictMode, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Home from "./pages/Home";
 import RecentMatches from "./pages/RecentMatches";
 import MatchDetail from "./pages/MatchDetail";
 import TDMLeaderboard from "./components/LeaderboardComponents/TDMLeaderboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import StandardLeaderboard from "./components/LeaderboardComponents/StandardLeaderboard";
 
 createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recent_matches" element={<RecentMatches />}></Route>
-            <Route path="/matches/:id" element={<MatchDetail />} />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<StandardLeaderboard />} />
+        <Route path="/tdm" element={<TDMLeaderboard />} />
+        <Route path="/recent_matches" element={<RecentMatches />} />
+        <Route path="/matches/:id" element={<MatchDetail />} />
+      </Routes>
     </Router>
+  </StrictMode>
 );
--- src/components/MatchComponents/MatchButton.tsx
+++ src/components/MatchComponents/MatchButton.tsx
@@ -1,20 +1,19 @@
 import { useNavigate } from "react-router-dom";
 
 interface MatchData {
-    _id: string;
-    map_name: string;
-    match_id: string;
+  _id: string;
+  map_name: string;
+  match_id: string;
 }
 
-
 const MatchButton = ({ matchData }: { matchData: MatchData }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <button onClick={() => navigate(`/matches/${matchData.match_id}`)}>
-            {matchData.map_name}
-        </button>
-    );
+  return (
+    <button onClick={() => navigate(`/matches/${matchData.match_id}`)}>
+      {matchData.map_name}
+    </button>
+  );
 };
 
 export default MatchButton;
